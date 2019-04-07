df87e95cd0f1d033dd698a2250dfd2236a6cb8ab
victor40000 <victor40000@yandex.ru>
2019-04-07 08:37:41 +0300

    canvas added

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-	width = 100;
-    height = 100;
+	width = 500;
+    height = 500;
 	var cnv = document.createElement('canvas');
 	cnv.width = width;
 	cnv.height = height;
+	// cnv.id = "someId"
+	body = document.getElementsByTagName('body')[0];
+	body.appendChild(cnv);
+	context = cnv.getContext('2d');
+	context.strokeRect(15, 15, 266, 266);
 	console.log("123");
 });
 
