9c0ec4f8eeb1f07b36d757320469caab33ec79cd
victor40000 <victor40000@yandex.ru>
2019-04-07 08:26:49 +0300

    script connected

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	width = 100;
+    height = 100;
+	var cnv = document.createElement('canvas');
+	cnv.width = width;
+	cnv.height = height;
+	console.log("123");
+});
+
+        
