2e489cf930612a5dad116546c6be2bc1e7a16768
victor40000 <victor40000@yandex.ru>
2019-04-07 11:21:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,25 @@ $(document).ready(function(){
 	body.appendChild(cnv);
 	context = cnv.getContext('2d');
 	context.strokeRect(15, 15, 266, 266);
-	var data = httpGet("https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=ru");
-	// var quote = data.quoteText;
-	console.log(data.quoteText);
+	var data = JSON.parse(httpGet("https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=ru"));
+	var quote = data.quoteText;
+	console.log(quote);
+
+	var pictures = new Array(4);
+    for (var i = 0; i < 4; i++) {
+        pictures[i] = new Image();
+        pictures[i].src = 'https://source.unsplash.com/collection/112716' + (3 + i) +'/' + width +'x'+ height;
+        // console.log('https://source.unsplash.com/collection/112716'+ (3 + i) +'/'+ canvaWidth +'x'+ canvaHeight);
+    }
+
+	setTimeout(function() { 
+		context.drawImage(pictures[0], -250, -250);
+		context.drawImage(pictures[1], -250, 250)
+		context.drawImage(pictures[0], 250, -250)
+		context.drawImage(pictures[1], 250, 250)
+	}, 2000);
+
+	
 
 	function httpGet(theUrl)
 	{
